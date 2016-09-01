dd71c1ef770038bc4176f7ae22cd1fa1321dddbc
Shuang Jiang <shane030716@hotmail.com>
2016-09-01 10:59:13 -0400

    commit in submodule

--- HistogramPrinter.java
+++ HistogramPrinter.java
@@ -1,5 +1,60 @@
 /*
  * Histogram Printer
+ * 
+ * HistogramPrinter prints out a histogram represented by an array of non-negative integers, 
+ * where each integer in the array represents the height of each bar.
+ * 
+ * This class has three public APIs for far
+ * 1. printHistogram(int[] arr)
+ * Example: input [1,0,0,0,6,0,0,3,10,2,3,4,5,0,3,0]
+ * Output: 
+ *                                  ___                            
+ *                                 |   |                           
+ *                                 |   |                           
+ *                                 |   |                           
+ *                  ___            |   |                           
+ *                 |   |           |   |            ___            
+ *                 |   |           |   |        ___|   |           
+ *                 |   |        ___|   |    ___|   |   |    ___    
+ *                 |   |       |   |   |___|   |   |   |   |   |   
+ *  ___            |   |       |   |   |   |   |   |   |   |   |   
+ * |___|___ ___ ___|___|___ ___|___|___|___|___|___|___|___|___|___
+ *   1   0   0   0   6   0   0   3  10   2   3   4   5   0   3   0
+ *   
+ * 2. printWaterFilledHistogram(int[] arr)
+ * Image pouring water into the histogram, what is the maximum amount water it can hold
+ * Example: input [1,0,0,0,6,0,0,3,10,2,3,4,5,0,3,0]
+ * Output: (+ represents water)
+ *                                  ___                            
+ *                                 |   |                           
+ *                                 |   |                           
+ *                                 |   |                           
+ *                  ___            |   |                           
+ *                 |   |+++ +++ +++|   |            ___            
+ *                 |   |+++ +++ +++|   |+++ +++ ±±±|   |           
+ *                 |   |+++ +++ ±±±|   |+++ ±±±|   |   |    ___    
+ *                 |   |+++ +++|   |   |±±±|   |   |   |+++|   |   
+ *  ___            |   |+++ +++|   |   |   |   |   |   |+++|   |   
+ * |___|±±± ±±± ±±±|___|±±± ±±±|___|___|___|___|___|___|±±±|___|___
+ *   1   0   0   0   6   0   0   3  10   2   3   4   5   0   3   0
+ *   
+ * 3. printLargestRectangleFilledHistogram(int[] arr)
+ * Fill the largest rectangle area that is can be formed by a number of contiguous bars, assuming the width of each bar is 1
+ * Example: input [1,0,0,0,6,0,0,3,10,2,3,4,5,0,3,0]
+ * Output: (+ represents the filled area)
+ *                                  ___                            
+ *                                 |   |                           
+ *                                 |   |                           
+ *                                 |   |                           
+ *                  ___            |   |                           
+ *                 |   |           |   |            ___            
+ *                 |   |           |   |        ___|   |           
+ *                 |   |        ___|   |    ___|   |   |    ___    
+ *                 |   |       |   |   |___|   |   |   |   |   |   
+ *  ___            |   |       |+++|+++|+++|+++|+++|+++|   |   |   
+ * |___|___ ___ ___|___|___ ___|±±±|±±±|±±±|±±±|±±±|±±±|___|___|___
+ *   1   0   0   0   6   0   0   3  10   2   3   4   5   0   3   0 
+ * 
  */
 
 
@@ -98,6 +153,15 @@ public class HistogramPrinter {
 			}
 		}
 		printHistogram(arr, null, filled);
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = new int[] {1,0,0,0,6,0,0,3,10,2,3,4,5,0,3,0};
+		HistogramPrinter printer = new HistogramPrinter();
+		printer.printHistogram(arr);
+		printer.printWaterFilledHistogram(arr);
+		printer.printLargestRectangleFilledHistogram(arr);
 		
 	}
 	
---
+++ README.md
@@ -0,0 +1,75 @@
+# Histogram Printer
+
+HistogramPrinter prints out a histogram represented by an array of non-negative integers, 
+where each integer in the array represents the height of each bar.
+
+This class has three public APIs for far
+
+`printHistogram(int[] arr)`
+
+Example: 
+
+input: `[1,0,0,0,6,0,0,3,10,2,3,4,5,0,3,0]`
+
+Output: 
+```
+                                 ___                            
+                                |   |                           
+                                |   |                           
+                                |   |                           
+                 ___            |   |                           
+                |   |           |   |            ___            
+                |   |           |   |        ___|   |           
+                |   |        ___|   |    ___|   |   |    ___    
+                |   |       |   |   |___|   |   |   |   |   |   
+ ___            |   |       |   |   |   |   |   |   |   |   |   
+|___|___ ___ ___|___|___ ___|___|___|___|___|___|___|___|___|___
+  1   0   0   0   6   0   0   3  10   2   3   4   5   0   3   0
+```  
+`printWaterFilledHistogram(int[] arr)`
+
+Image pouring water into the histogram, what is the maximum amount water it can hold.
+
+Example: 
+
+input: `[1,0,0,0,6,0,0,3,10,2,3,4,5,0,3,0]`
+
+Output: (+ represents water)
+```
+                                 ___                            
+                                |   |                           
+                                |   |                           
+                                |   |                           
+                 ___            |   |                           
+                |   |+++ +++ +++|   |            ___            
+                |   |+++ +++ +++|   |+++ +++ ±±±|   |           
+                |   |+++ +++ ±±±|   |+++ ±±±|   |   |    ___    
+                |   |+++ +++|   |   |±±±|   |   |   |+++|   |   
+ ___            |   |+++ +++|   |   |   |   |   |   |+++|   |   
+|___|±±± ±±± ±±±|___|±±± ±±±|___|___|___|___|___|___|±±±|___|___
+  1   0   0   0   6   0   0   3  10   2   3   4   5   0   3   0
+```
+
+`printLargestRectangleFilledHistogram(int[] arr)`
+
+Fill the largest rectangle area that is can be formed by a number of contiguous bars, assuming the width of each bar is 1
+
+Example: 
+
+input: `[1,0,0,0,6,0,0,3,10,2,3,4,5,0,3,0]`
+
+Output: (+ represents the filled area)
+```
+                                 ___                            
+                                |   |                           
+                                |   |                           
+                                |   |                           
+                 ___            |   |                           
+                |   |           |   |            ___            
+                |   |           |   |        ___|   |           
+                |   |        ___|   |    ___|   |   |    ___    
+                |   |       |   |   |___|   |   |   |   |   |   
+ ___            |   |       |+++|+++|+++|+++|+++|+++|   |   |   
+|___|___ ___ ___|___|___ ___|±±±|±±±|±±±|±±±|±±±|±±±|___|___|___
+  1   0   0   0   6   0   0   3  10   2   3   4   5   0   3   0 
+```
